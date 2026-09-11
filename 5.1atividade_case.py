9e7955b866f6e3e20c2d78a3b16e300c60e83fe1
joaovsilva60-pixel <joao.v.silva60@ba.estudante.senai.br>
2026-09-11 16:43:48 -0300

    Add files via upload

---
+++ 5.1atividade_case.py
@@ -0,0 +1,9 @@
+import os
+os.system('cls')
+
+#SOLICITANDO DADOS
+altura = float(input('Digite a altura : '))
+print('A altura digitada foi : ', altura)
+peso = float(input('Digite o peso : '))
+print('O peso digitado foi : ', peso)
+
